5bb8feb011e5401137ba3f77cc258bcf3ea731fe
Viktoriia Luchyk <vluchyk@prosoftalliance.com>
2023-12-29 16:49:22 +0200

    init commit

---
+++ src/main/java/com/gmail/luchyk/viktoriia/glovo/GlovoApplication.java
@@ -0,0 +1,13 @@
+package com.gmail.luchyk.viktoriia.glovo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GlovoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GlovoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gmail/luchyk/viktoriia/glovo/GlovoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gmail.luchyk.viktoriia.glovo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GlovoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
